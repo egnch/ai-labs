01a3680e4e974b889b0772de49e5ad39c6c2ff8f
egnch <egn.ch@yandex.ru>
2024-10-21 11:36:44 +0300

    fix: enable html mode

--- lab1/app/__init__.py
+++ lab1/app/__init__.py
@@ -21,7 +21,7 @@ def create_app() -> FastAPI:
     app.add_event_handler("startup", get_translation_service_startup(app))
     app.add_event_handler("shutdown", get_openai_service_shutdown(app))
     
-    app.mount("/app", StaticFiles(directory="static"), name="app")
+    app.mount("/app", StaticFiles(directory="static", html=True), name="app")
     app.include_router(router, prefix="/api")
 
     return app
